e33008e67b0ac0459bfe9bf9400702c39d1cbe80
Sébastien Chopin <seb@nuxtlabs.com>
2023-05-23 11:06:53 +0200

    chore(app): initial commit by Nuxt Studio

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  cover: '/cover.jpg',
+  socials: {
+    twitter: 'Atinux',
+    github: 'Atinux/content-wind'
+  },
+  prose: {
+    h1: {
+      icon: 'carbon:sailboat-coastal'
+    }
+  }
+})
